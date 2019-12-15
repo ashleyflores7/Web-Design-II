ee66db545b819d17f7b81317fe80f7062484ec1d
Ashley Flores <ashleyj.ff@gmail.com>
2019-12-15 17:17:11 -0600

    final done

--- js/main.js
+++ js/main.js
@@ -3,10 +3,6 @@ $(document).ready(function () {
     $("#button").click(function () {
     $("#hello").removeClass('hello').addClass('button');
     });
-
-    $("#button").click(function () {
-        $("#goodbye").removeClass('button');
-    });
     
     setTimeout(function () {
     $('#hello').addClass('h1');
